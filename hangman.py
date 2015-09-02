40354837a9635cf6ddbf0d49d5a0cf88c09c67b4
John Richter <john@johnathanrichter.com>
2015-09-02 09:53:03 -0400

    Initial commit

---
+++ hangman.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, url_for, session, redirect
+
+app = Flask(__name__)
+
+DATABASE = ''
+
+# Webpages
+#  index/root view
+#  login
+#  game page
+#  ability to log out
+#  if time: win/loss records per user view
+
+# Functions
+#  Database handler
+#  login
+#  logout
+#  connection/session manager to update clients of the game status
+
